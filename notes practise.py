d19c85abc08e0123ac70efb79bd508145b1f3710
Rajshreethapa7 <rajshreethapa7@gmail.com>
2026-02-11 20:29:54 -0600

    work

---
+++ notes practise.py
@@ -0,0 +1,60 @@
+x = 5
+y = "john"
+print(x)
+print(y)
+
+###get a type of a variable
+
+x= 5
+y = "john"
+print(type(x))
+print(type(y))
+
+##assigning variable
+
+x,y,z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+
+## assign value as a list
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+## print function (ways of print function can be used
+
+x= "python"
+y = "is"
+z = "awesome"
+print(x,y,z)
+
+##global variable
+x = "awesome"
+def myfunc():
+     print("I am " + x)
+myfunc()
+
+## Data types
+# slicing strings
+
+b = "Hello Rajshree"
+print(b[::])
+print(b.upper())
+print(b.lower())
+print(b.strip())
+
+##List
+
+list = ["apple", "banana", "cherry"]
+print(len(list))
+
+#list while loop
+
+list = ["apple", "banana", "cherry"]
+i = 0
+while i < len(list):
+    print(list[i])
+    i = i + 1
+print(i)
+
+#Dictionary
